f1259f7efe23d8c29307bc20e4a4ab185c8f06b0
Jovan <j.xavier1076@gmail.com>
2024-11-11 23:04:04 +0000

    merged master

--- DNA-Sequencing/dna-seq.sh
+++ DNA-Sequencing/dna-seq.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-cd ~/DNA-Sequencing
-
-bowtie2-build reference_genome/GCF_000006765.1_ASM676v1_genomic.fna reference_genome/Bowtie2Idx/ASM676v1.108
-
-mkdir QC
-fastqc -o QC/ raw_fastq/*
-
-conda create --name multiqc -c bioconda multiqc
-conda activate multiqc
-multiqc QC/ -o QC
-conda deactivate
-
-mkdir Alignment
-
-time bowtie2 -p 1 \
-    -x reference_genome/Bowtie2Idx/ASM676v1.108 \
-    -1 raw_fastq/ERR3697100_1.fastq.gz \
-    -2 raw_fastq/ERR3697100_2.fastq.gz |
-    samtools sort -o Alignment
-=======
 # Sequencing pipeline
 # 1. QC FASTQ files
 # 2. Align to genome
@@ -147,4 +126,3 @@ fi
 done
 
 echo "Finished"
->>>>>>> master
