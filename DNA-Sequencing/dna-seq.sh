11a411abf0b92ffc23a4caaa5473fd6740b499c2
Jovan Xavier <j.xavier1076@gmail.com>
2025-01-24 00:59:30 +0000

    DNA-seq + Changes

--- DNA-Sequencing/dna-seq.sh
+++ DNA-Sequencing/dna-seq.sh
@@ -1,110 +1,201 @@
 #!/bin/bash
 
-# Sequencing pipeline
-# 1. QC FASTQ files
-# 2. Align to genome
-# 3. Mark duplicates
+set -e # exit if error
 
-read1=$1
-read1name=$"${read1%.*}"
-read2=$2
-read2name=$"${read2%.*}"
-reference=$3
-cores=$4
-ramAllocation=$5
-known_sites=$6
+check_mkdir() {
+    if [ ! -d $1 ]; then
+        mkdir $1
+    fi
+}
 
-# Create folder for DNA Sequencing
-if [ ! -d DNA_seq ]; then
-    mkdir DNA_seq
+check_mkdir DNA_seq_analysis
+cd DNA_seq_analysis
+cp -vR /data/teaching/bci_teaching/DNAseq/VarScan.v2.4.3.jar ./
+HUMANDB_PATH="/data/teaching/bci_teaching/DNAseq/Reference/humandb/"
+VARSCAN=VarScan.v2.4.3.jar
+
+# Default message
+usage() {
+    echo $0 -r <FASTQ_R1> -s <FASTQ_R2> -t <REFERENCE> -u <THREADS> -w <KNOWN_VARIANTS>
+    exit 1
+}
+
+# Parse arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -r|--read1)
+            FASTQ_R1=$2
+            shift 2
+            ;;
+        -s|--read2)
+            FASTQ_R2=$2
+            shift 2
+            ;;
+        -t|--reference)
+            REFERENCE=$2
+            shift 2
+            ;;
+        -u|--threads)
+            THREADS=$2
+            shift 2
+            ;;
+        -v|--ram_usage)
+            RAM=$2
+            shift 2
+            ;;
+        -w|--known_variants)
+            KNOWN_VARIANTS=$2
+            shift 2
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+# Ensure required arguments are provided
+if [[ -z $FASTQ_R1 || -z $FASTQ_R2 || -z $REFERENCE || -z $THREAD || -z $RAM || -z $KNOWN_VARIANTS ]]; then
+    usage
 fi
-cd DNA_seq
 
-# Create folder to store all logs
-if [ ! -d logs ]; then
-    mkdir logs
-fi
+# Derive file names from the input files
+BASENAME_R1=$(basename "$FASTQ_R1" .fastq.gz)
+BASENAME_R2=$(basename "$FASTQ_R2" .fastq.gz)
+PREFIX=${BASENAME_R1%%_*}
 
-# 1. QC FASTA files
+# Output files
+BAM_FILE=Alignment/${PREFIX}.bam
+MARKED_FILE=QC/${PREFIX}.marked
+MARKED_BAM=Alignment/${PREFIX}.marked.bam
+RECALIBRATED_BAM=Alignment/${PREFIX}.recalib.bam
+TABLE=Alignment/${PREFIX}.table
+VCF_FILE=VCF/${PREFIX}.vcf
+MULTIANNO=VCF/VCF/${PREFIX}.hg38_multianno.txt
 
-# Create folder for QC files
-if [ ! -d QC ]; then
-    mkdir QC
-fi
+# Quality Control - FASTQC analysis
+check_mkdir QC
 
-# Generating FASTQC analysis
+# Step 1: Quality Control
+echo "Step 1: Quality Control"
+
+module load fastqc anaconda3
+conda create --name multiqc -c bioconda multiqc -y
+
 if [ ! -f QC/multiqc_report.html ]; then
-    module load fastqc
-    module load anaconda3
-
-    conda create --namae multiqc -c bioconda multiqc -y
     conda activate multiqc
-
-    {
-        fastqc -o QC/ read1
-        fastqc -o QC/ read2
-        multiqc QC/ -o QC
-    } &> logs/fastqc_logs.txt
-
-    conda deactivate multiqc
+    fastqc $FASTQ_R1 $FASTQ_R2 -o QC/ -t $THREADS
+    multiqc QC/ -o QC
+    conda deactivate
 fi
+module unload fastqc
 
-# 2. Align to genome
+# Step 2: Read Alignment
+echo "Step 2: Read Alignment"
+check_mkdir Alignment
 
-# Create Bowtie2Idk
-module load Bowtie2
-if [ ! -d Bowtie2Idx ]; then
-    mkdir Bowtie2Idx
-fi
-bowtie2-build reference Bowtie2IDx/reference.108
-
-# Create folder for alignment
-if [ ! -d Alignment ]; then
-    mkdir Alignment
-fi
-
-if [ ! -f Aligment/${read1name}.bam ]; then
-    {
-    bowtie2 -p cores \
-        --rg ID:$read1name \
-        --rg SM:$read1name \
+module load bowtie2
+if [ ! -f $BAM_FILE ]; then
+    bowtie2 -p $THREADS \
+        --rg ID:$PREFIX \
+        --rg SM:$PREFIX \
         --rg PL:ILLUMINA \
-        --rg LB:$read1name \
-        -x Bowtie2IDx/reference.108 \
-        -1 read1 \
-        -2 read2 |
-        samtools sort -o Aligment/${read1name}.bam -
-    } &> logs/read1name_alignment_logs.txt
+        --rg LB:$PREFIX \
+        -x $REFERENCE \
+        -1 $FASTQ_R1 \
+        -2 $FASTQ_R2 |
+        samtools sort -o $BAM_FILE -
+fi
+module unload bowtie2
+
+# Step 3: Mark Duplicates
+echo "Step 3: Mark Duplicates"
+
+module load gatk
+if [ ! -f $MARKED_BAM ]; then
+        gatk --java-options "-Xmx${RAM}G" MarkDuplicates \
+                -I $BAM_FILE \
+                -M $MARKED_FILE \
+                -O $MARKED_BAM
 fi
 
-# 3. Mark Duplicates
-if [ ! -f Alignment/${read1name}.marked.bam ]; then
-    {
-        gatk --java-options "-Xmx10G" MarkDuplicates \
-                -I Alignment/${read1name}.bam \
-                -M QC/${read1name}.marked \
-                -O Alignment/${read1name}.marked.bam
-    } &> logs/read1name_marked_logs.txt
+# Step 4: Base Score Quality Recalibration
+echo "Step 4: Base Score Quality Recalibration"
+
+if [ ! -f $RECALIBRATED_BAM ]; then 
+        gatk --java-options "-Xmx${RAM}G" BaseRecalibrator \
+                -I $MARKED_BAM \
+                -R  $REFERENCE \
+                --known-sites $KNOWN_VARIANTS \
+                -O $TABLE
+        gatk --java-options "-Xmx${RAM}G" ApplyBQSR \
+                -R $REFERENCE \
+                -I $MARKED_BAM \
+                --bqsr-recal-file $TABLE \
+                -O $RECALIBRATED_BAM
+fi
+module unload gatk
+
+# Step 5: Variant Calling
+echo "Step 5: Variant Calling"
+check_mkdir VCF
+
+module load java
+if [ ! -f $VCF_FILE ]; then
+    samtools mpileup \
+        -q 20 \
+        -f $REFERENCE \
+        $RECALIBRATED_BAM |
+    java -jar VARSCAN mpileup2snp \
+        --min-coverage 20 \
+        --min-avg-qual 20 \
+        --min-read2 4 \
+        --p-value 0.2 \
+        --min-var-freq 0.01 \
+        --strand-filter 1 \
+        --output-vcf 1 > $VCF_FILE
 fi
 
-# 4. Run Base Score Quality Recalibration
-if [ ! -f Alignment/${read1name}.recalib.bam ]; then 
-        {
-        gatk --java-options "-Xmx10G" BaseRecalibrator \
-                -I Alignment/${read1name}.marked.bam \
-                -R  $reference \
-                --known-sites ${known_sites} \
-                -O Alignment/${read1name}.table
+# Step 6: Annotate Variants
+echo "Step 6: Annotate Variants"
 
-        gatk --java-options "-Xmx10G" ApplyBQSR \
-                -R $reference \
-                -I Alignment/${read1name}.marked.bam \
-                --bqsr-recal-file Alignment/${read1name}.table \
-                -O Alignment/${read1name}.recalib.bam
-        } &> logs/${read1name}_BSQR_logs.txt
-fi
+module load annovar
+if [ ! -f $PASS_VCF ]; then
+    convert2annovar.pl --format vcf4 \
+        $VCF_FILE \
+        --includeinfo \
+        --filter PASS \
+        --outfile $PASS_VCF
 
-# Running Flagstat analysis
-if [ ! -f logs/${read1name}_alignment_flagstat.txt ]; then 
-        samtools flagstat Alignment/${read1name}.recalib.bam > logs/${read1name}_alignment_flagstat.txt
-fi
+    # Filter mutations with MAF > 0.01 from the 1000 Genomes Project
+    annotate_variation.pl -filter \
+        -dbtype 1000g2015aug_all \
+        -buildver hg38 \
+        -out VCF/${PREFIX} \
+        $PASS_VCF \
+        $HUMANDB_PATH \
+        -maf 0.01
+
+    # Filter mutations with MAF > 0.01 from the Exome Sequencing Project
+    annotate_variation.pl -filter \
+        -dbtype esp6500siv2_all \
+        -buildver hg38 \
+        -out VCF/${PREFIX} \
+        VCF/${PREFIX}.hg38_ALL.sites.2015_08_filtered \
+        "$HUMANDB_PATH" \
+        -score_threshold 0.01
+
+    # Annotate the variants with RefGene, dbSNP, COSMIC, and Cytoband information
+    table_annovar.pl \
+        -buildver hg38 \
+        -out VCF/${PREFIX} \
+        VCF/${PREFIX}.hg38_esp6500siv2_all_filtered \
+        $HUMANDB_PATH \
+        -remove \
+        -otherinfo \
+        -protocol refgene,avsnp150,cosmic92_coding,cytoband \
+        -operation g,f,f,r \
+        -nastring .
+
+# Step 7: Filter out variants with variant allele frequency <10% using R script 
+module load R
+Rscript dna-seq-filter.R $MULTIANNO
